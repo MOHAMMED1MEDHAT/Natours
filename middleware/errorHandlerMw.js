1a9b61b125843db70eef72768bab6ab40af1422e
mohammed medhat <mohamed.medhat2121@gmail.com>
2023-07-07 10:56:43 +0300

    Error Handling 0.6

--- middleware/errorHandlerMw.js
+++ middleware/errorHandlerMw.js
@@ -11,6 +11,12 @@ const handleDuplicateFieldsDB = (err) => {
     return new AppError(message, 400);
 };
 
+const handleValidationErrorDB = (err) => {
+    const errors = Object.values(err.errors).map((el) => el.message);
+    const message = `Invalid input data: ${errors.join('. ')}`;
+    return new AppError(message, 400);
+};
+
 sendErrorDev = (err, res) => {
     res.status(500).json({
         status: 'fail',
@@ -39,6 +45,8 @@ module.exports = (err, res) => {
         let error = { ...err };
         if (error.name === 'CastError') error = handleCastErrorDB(error);
         if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+        if (error.name === 'ValidationError')
+            error = handleValidationErrorDB(error);
         sendErrorProd(error, res);
     }
 };
