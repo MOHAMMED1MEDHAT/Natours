3fd194244f4a10ae9e01c6551abd5bd97aabff85
mohammed medhat <mohamed.medhat2121@gmail.com>
2023-07-12 12:12:05 +0300

    Added password hashing 0.2

--- controller/authController.js
+++ controller/authController.js
@@ -1,8 +1,23 @@
 const User = require('../model/userModel');
 const catchAsync = require('./../util/catchAsync');
 
+const jwt = require('jsonwebtoken');
+
 exports.signup = catchAsync(async (req, res, next) => {
-    const newUser = await User.create(req.body);
+    const { name, email, password, passwordConfirm } = req.body;
+    const newUser = await User.create({
+        name,
+        email,
+        password,
+        passwordConfirm,
+    });
+
+    const token = jwt.sign(
+        {
+            userId: newUser._id,
+        },
+        process.env.JWT_SECRET
+    );
 
     res.status(200).json({
         status: 'success',
