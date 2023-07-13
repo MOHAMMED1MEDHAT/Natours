05881d2deb2a459595cec1dd2f945d0d14855bf3
mohammed medhat <mohamed.medhat2121@gmail.com>
2023-07-13 09:23:33 +0300

    Changed User Model To Have Roles 0.1

--- model/userModel.js
+++ model/userModel.js
@@ -23,6 +23,11 @@ const userSchema = new mongoose.Schema(
         photo: {
             type: String,
         },
+        role: {
+            type: String,
+            enum: ['user', 'guide', 'lead-guide', 'admin'],
+            default: 'user',
+        },
         password: {
             type: String,
             required: [true, 'Please provide a password'],
