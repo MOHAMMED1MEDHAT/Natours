c52d0be288ae128dfd4fff5905e3f82f585ebb39
mohammed medhat <mohamed.medhat2121@gmail.com>
2023-07-16 20:16:06 +0300

    Fixed render issue 0.1

--- controller/userController.js
+++ controller/userController.js
@@ -63,21 +63,21 @@ exports.getUser = catchAsync(async (req, res) => {
         message: 'This route is not yet defined!',
     });
 });
-// exports.createUser = (req, res) => {
-//     res.status(500).json({
-//         status: 'error',
-//         message: 'This route is not yet defined!',
-//     });
-// };
-// exports.updateUser = (req, res) => {
-//     res.status(500).json({
-//         status: 'error',
-//         message: 'This route is not yet defined!',
-//     });
-// };
-// exports.deleteUser = (req, res) => {
-//     res.status(500).json({
-//         status: 'error',
-//         message: 'This route is not yet defined!',
-//     });
-// };
+exports.createUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not yet defined!',
+    });
+};
+exports.updateUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not yet defined!',
+    });
+};
+exports.deleteUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not yet defined!',
+    });
+};
