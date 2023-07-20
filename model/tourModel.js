c1f4c40c237dca73bd8796ecca4c968a21ba3888
mohammed medhat <mohamed.medhat2121@gmail.com>
2023-07-20 11:08:12 +0300

    Added Geospatial Atts 0.1

--- model/tourModel.js
+++ model/tourModel.js
@@ -80,6 +80,29 @@ const TourSchema = new mongoose.Schema(
             type: Boolean,
             default: false,
         },
+        startLocation: {
+            type: {
+                type: String,
+                default: 'Point',
+                enum: ['Point'],
+            },
+            coordinates: [Number],
+            address: String,
+            description: String,
+        },
+        locations: [
+            {
+                type: {
+                    type: String,
+                    default: 'Point',
+                    enum: ['Point'],
+                },
+                coordinates: [Number],
+                address: String,
+                description: String,
+                day: Number,
+            },
+        ],
     },
     {
         toJSON: { virtuals: true },
