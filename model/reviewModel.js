67902440b176f914f0c4aecee703d14f472857ce
mohammed medhat <mohamed.medhat2121@gmail.com>
2023-07-27 14:38:50 +0300

    update review model 0.1

--- model/reviewModel.js
+++ model/reviewModel.js
@@ -1,27 +1,35 @@
 const mongoose = require('mongoose');
 
-const reviewSchema = new mongoose.Schema({
-    author: {
-        type: mongoose.Types.ObjectId,
-        ref: 'user',
+const reviewSchema = new mongoose.Schema(
+    {
+        author: {
+            type: mongoose.Types.ObjectId,
+            ref: 'user',
+            required: [true, 'a review must have an author'],
+        },
+        tour: {
+            type: mongoose.Types.ObjectId,
+            ref: 'tour',
+            required: [true, 'a review must have a tour'],
+        },
+        review: {
+            type: String,
+            required: [true, 'Can not have no review'],
+        },
+        rating: {
+            type: Number,
+            min: [1, 'no less than one'],
+            max: [5, 'no more than five'],
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now(),
+        },
     },
-    tour: {
-        type: mongoose.Types.ObjectId,
-        ref: 'tour',
-    },
-    review: {
-        type: String,
-        required: [true, 'Can not have no review'],
-    },
-    rating: {
-        type: Number,
-        min: [1, 'no less than one'],
-        max: [5, 'no more than five'],
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now(),
-    },
-});
+    {
+        toJSON: { virtuals: true },
+        toObject: { virtuals: true },
+    }
+);
 
 module.exports = mongoose.model('review', reviewSchema);
