0a727066ce0a8aa0af4958bf3962fd8730af107a
mohammed medhat <mohamed.medhat2121@gmail.com>
2023-08-04 22:04:45 +0300

    Refactored for factory handler 0.6

--- controller/handlerFactory.js
+++ controller/handlerFactory.js
@@ -32,3 +32,13 @@ exports.updateOne = (Model) =>
             data: { doc },
         });
     });
+
+exports.createOne = (Model) =>
+    catchAsync(async (req, res, next) => {
+        const doc = await Model.create({ ...req.body });
+
+        res.status(200).json({
+            status: 'success',
+            data: { doc },
+        });
+    });
