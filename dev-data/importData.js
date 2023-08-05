6f9935fcfd991c29da1751dec7f16aa093835831
mohammed medhat <mohamed.medhat2121@gmail.com>
2023-08-05 11:27:52 +0300

    Refactored date imports router 0.1

--- dev-data/importData.js
+++ dev-data/importData.js
@@ -1,10 +1,12 @@
 require('dotenv').config({ path: './../config.env' });
 const fs = require('fs');
 const Tour = require('./../model/tourModel');
+const Review = require('./../model/reviewModelModel');
+const User = require('./../model/userModel');
 const mongoose = require('mongoose');
 
 mongoose
-    .connect(process.env.LOCAL_CONNECTION_STRING, {
+    .connect(process.env.ATLAS_CONNECTION_STRING, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         dbName: 'Natours',
@@ -31,6 +33,54 @@ const importData = async () => {
 const deleteData = async () => {
     try {
         await Tour.deleteMany();
+        console.log('deleted all');
+        process.exit();
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+const reviews = JSON.parse(
+    fs.readFileSync(`${__dirname}/data/reviews.json`, 'utf-8')
+);
+
+const importReviewData = async () => {
+    try {
+        await Review.create(reviews);
+        console.log('added all');
+        process.exit();
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+const deleteReviewData = async () => {
+    try {
+        await Review.deleteMany();
+        console.log('deleted all');
+        process.exit();
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+const users = JSON.parse(
+    fs.readFileSync(`${__dirname}/data/users.json`, 'utf-8')
+);
+
+const importUserData = async () => {
+    try {
+        await User.create(users);
+        console.log('added all');
+        process.exit();
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+const deleteUserData = async () => {
+    try {
+        await User.deleteMany();
         console.log('deleted all');
         process.exit();
     } catch (err) {
