c666d6bf98c375cdae365bdf0db091c92fbb1c8a
mohammed medhat <mohamed.medhat2121@gmail.com>
2023-08-21 22:48:13 +0300

    Added tour photo upload feature 0.1

--- controller/tourController.js
+++ controller/tourController.js
@@ -1,7 +1,35 @@
+const multer = require('multer');
+const sharp = require('sharp');
 const Tour = require('../model/tourModel');
 const AppError = require('../util/appError');
 const catchAsync = require('./../util/catchAsync');
 const factory = require('./handlerFactory');
+
+const multerStorage = multer.memoryStorage();
+
+const multerFilter = (req, file, cb) => {
+    if (file.mimetype.startsWith('image')) {
+        cb(null, true);
+    } else {
+        cb(new AppError('Not an image try to upload an image', 400), false);
+    }
+};
+
+const upload = multer({
+    storage: multerStorage,
+    fileFilter: multerFilter,
+});
+
+exports.uploadTourImages = upload.fields([
+    {
+        name: 'imageCover',
+        maxCount: 1,
+    },
+    {
+        name: 'images',
+        maxCount: 5,
+    },
+]);
 
 exports.getAllTours = factory.getAll(Tour);
 
