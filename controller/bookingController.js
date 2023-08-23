c443a4bb2aa8c0dbc32d46e3219d07c2a0ed4ec9
mohammed medhat <mohamed.medhat2121@gmail.com>
2023-08-23 15:39:15 +0300

    Adding stripe payment feature 0.7

--- controller/bookingController.js
+++ controller/bookingController.js
@@ -7,7 +7,9 @@ const AppError = require('./../util/appError');
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     const tour = await Tour.findById(req.params.tourID);
 
-    stripe.checkout.session.create({
+    console.log(process.env.STRIPE_SECRTE_KEY);
+
+    const session = await stripe.checkout.sessions.create({
         payment_method_types: ['card'],
         success_url: `${req.protocol}://${req.get('host')}/?tour=${
             req.params.tourID
