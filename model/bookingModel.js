31d86b7af473b1993deb51f01990aea4461e216e
mohammed medhat <mohamed.medhat2121@gmail.com>
2023-08-23 16:03:44 +0300

    Adding booking feature 0.1

---
+++ model/bookingModel.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const bookingSchema = new mongoose.Schema(
+    {
+        tour: {
+            type: mongoose.Schema.ObjectId,
+            ref: 'Tour',
+            required: [true, 'booking must belong to a tour'],
+        },
+        user: {
+            type: mongoose.Schema.ObjectId,
+            ref: 'User',
+            required: [true, 'booking must belong to a user'],
+        },
+        price: {
+            type: Number,
+            required: [true, 'booking must have a price'],
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now(),
+        },
+        paid: {
+            type: Boolean,
+            default: true,
+        },
+    },
+    {
+        toJSON: { virtuals: true },
+        toObject: { virtuals: true },
+    }
+);
+
+bookingSchema.pre(/^find/, function (next) {
+    this.populate('user').populate({
+        path: 'tour',
+        select: 'name',
+    });
+    next();
+});
+
+const Booking = mongoose.model('Booking', bookingSchema);
